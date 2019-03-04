fcac0fcd499c59531fe23e65c8dac36de4869a96
iKurisu <chris.epic01@gmail.com>
2019-03-04 09:34:35 +0100

    Prevent Sections scroll in some situations

--- src/Sections/index.js
+++ src/Sections/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Section from './Section'
+import "./styles.scss";
 
 let key = 0;
 
-const Sections = ({ artworks }) => (
-  <div className="Sections">
+const Sections = ({ artworks, atIntro }) => (
+  <div className={`Sections ${atIntro ? 'no-scroll' : ''}`}>
     { artworks.map(artwork => <Section {...artwork} key={key++} />) }
     <Section {...artworks[0]} />
   </div>
 )
 
-export default Sections;
+const mapStateToProps = ({ atIntro }) => ({ atIntro });
+
+export default connect(mapStateToProps)(Sections);
