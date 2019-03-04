fb356649abe160d4d988b3f37933352829f5e1c8
iKurisu <chris.epic01@gmail.com>
2019-03-04 09:16:33 +0100

    Add Intro component

---
+++ src/Intro/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles.scss";
+
+class Intro extends React.Component {
+  render() {
+    return (
+      <div className="intro-container">
+      
+      </div>
+    )
+  }
+}
+
+export default Intro;
