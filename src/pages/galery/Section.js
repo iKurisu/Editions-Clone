43a5de81f3ead9ea0cc43d016a0edda00a9f6e3f
iKurisu <chris.epic01@gmail.com>
2019-04-12 10:00:31 +0200

    Refactor class into hook

--- src/pages/galery/Section.js
+++ src/pages/galery/Section.js
@@ -1,38 +1,44 @@
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
+import Details from "./section/Details";
+import Image from "./section/Image";
+import Title from "./section/Title";
 import "./Section.scss";
 
-class Section extends React.Component {
-  state = { offX: 0, offY: 0 };
+const Section = ({ artwork, title, details, orientation, colors }) => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-  getOffset(position, size, range) {
+  const getOffset = (position, size, range) => {
     const percent = position * 100 / size;
     return range * percent / 100 - range / 2;
   }
 
-  mouseMove = e => {
+  const mouseMove = e => {
     const { clientX, clientY } = e;
     const { innerWidth, innerHeight } = window;
 
-    const offX = this.getOffset(clientX, innerWidth, 28);
-    const offY = this.getOffset(clientY, innerHeight, 18);
-
-    this.setState({ offX, offY });
+    setX(getOffset(clientX, innerWidth, 28));
+    setY(getOffset(clientY, innerHeight, 18));
   }
 
-  render() {
-    const { offX, offY } = this.state;
-    const { artwork, title, details, orientation, colors } = this.props;
+  return (
+    <section className={`section`} onMouseMove={mouseMove}>
+      <div className="content-wrapper">
+        <Image artwork={artwork} orientation={orientation} />
+        <Title
+          title={title}
+          style={{
+            transform: `translate(${x}px, ${y}px)`,
+            color: colors.font
+          }}
+        />
+        <Details {...details} />
+      </div>
+    </section>
+  );
+}
 
-    return (
-      <section className={`section`} onMouseMove={this.mouseMove}>
-        <div className="content-wrapper">
-          <Image title={title} artwork={artwork} orientation={orientation} />
-          <Title title={title} style={{ transform: `translate(${offX}px, ${offY}px)`, color: colors.font }} />
-          <Details {...details} />
-        </div>
-      </section>
-    );
-  }
 Section.propTypes = {
   artwork: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
