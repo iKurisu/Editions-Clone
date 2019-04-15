3e79c7c9cea5adc0ba2b7c77902b3ad81afdbea8
iKurisu <chris.epic01@gmail.com>
2019-04-15 20:31:43 +0200

    Pass atIntro prop down

--- src/pages/galery/Section.js
+++ src/pages/galery/Section.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Details from "./section/Details";
 import Image from "./section/Image";
 import Title from "./section/Title";
 import "./Section.scss";
 
-const Section = ({ artwork, title, details, orientation, colors }) => {
+const Section = ({ artwork, atIntro }) => {
+  const { src, orientation, title, colors, details } = artwork;
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
   const getOffset = (position, size, range) => {
-    const percent = position * 100 / size;
-    return range * percent / 100 - range / 2;
-  }
+    const percent = (position * 100) / size;
+    return (range * percent) / 100 - range / 2;
+  };
 
   const mouseMove = e => {
     const { clientX, clientY } = e;
@@ -20,12 +21,12 @@ const Section = ({ artwork, title, details, orientation, colors }) => {
 
     setX(getOffset(clientX, innerWidth, 28));
     setY(getOffset(clientY, innerHeight, 18));
-  }
+  };
 
   return (
     <section className={`section`} onMouseMove={mouseMove}>
       <div className="content-wrapper">
-        <Image artwork={artwork} orientation={orientation} />
+        <Image src={src} orientation={orientation} atIntro={atIntro} />
         <Title
           title={title}
           style={{
@@ -37,14 +38,11 @@ const Section = ({ artwork, title, details, orientation, colors }) => {
       </div>
     </section>
   );
-}
+};
 
 Section.propTypes = {
-  artwork: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  details: PropTypes.object.isRequired,
-  orientation: PropTypes.string.isRequired,
-  colors: PropTypes.object.isRequired
-}
+  artwork: PropTypes.object.isRequired,
+  atIntro: PropTypes.bool
+};
 
 export default Section;
