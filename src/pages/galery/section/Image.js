3386eb1fbe7690d4adc355aaf171191bfbc4c0bf
iKurisu <chris.epic01@gmail.com>
2019-04-15 20:32:19 +0200

    Add image transitions at intro

--- src/pages/galery/section/Image.js
+++ src/pages/galery/section/Image.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import "./Image.scss";
 
-const Image = ({ artwork, orientation }) => (
-  <div className={`image-wrapper ${orientation}`}>
-    <img src={artwork} />
-  </div>
-)
+const Image = ({ src, orientation, atIntro }) => {
+  const [scale, setScale] = useState(atIntro ? 1.06 : 1.03);
+  const [opacity, setOpacity] = useState(atIntro ? 0 : 1);
+
+  useEffect(() => {
+    setScale(atIntro ? 1.03 : 1);
+  })
+
+  useEffect(() => {
+    setOpacity(1);
+  }, []);
+
+  return (
+    <div
+      className={`image-wrapper ${orientation}`}
+      style={{ transform: `scale(${scale})`, opacity: opacity }}
+    >
+      <img src={src} />
+    </div>
+  );
+}
 
 Image.propTypes = {
-  artwork: PropTypes.string.isRequired,
-  orientation: PropTypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  orientation: PropTypes.string.isRequired,
+  atIntro: PropTypes.bool
 }
 
 export default Image;
