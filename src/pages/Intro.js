c92bcddd8530c099c5f1291b6b2d85d3894110a0
iKurisu <chris.epic01@gmail.com>
2019-04-16 16:08:10 +0200

    Add effect to delay style change

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import "./Intro.scss";
 
-const Intro = ({ showGalery, atIntro }) => (
-  <div 
-    className="intro-container" 
-    style={{ display: atIntro ? 'block' : 'none' }}
-    onClick={showGalery}
-  >
-    <div className="top text-center intro">
-      <p>Selected Artwork</p>
-    </div>
-    <div className="vertical-container">
-      <div className="headline-container">
-        <div className="headline">
-          <span>Editions</span>
+const Intro = ({ showGalery, atIntro }) => {
+  const [display, setDisplay] = useState('block');
+  
+  useEffect(() => {
+    !atIntro && setTimeout(() => setDisplay('none'), 900)
+  });
+  
+  return (
+    <div 
+      className="intro-container" 
+      style={{ display: display }}
+      onClick={showGalery}
+    >
+      <div className="top text-center intro">
+        <p>Selected Artwork</p>
+      </div>
+      <div className="vertical-container">
+        <div className="headline-container">
+          <div className="headline">
+            <span>Editions</span>
+          </div>
         </div>
       </div>
+      <div className="bottom text-center intro">
+        <p>2016 – Present</p>
+      </div>
     </div>
-    <div className="bottom text-center intro">
-      <p>2016 – Present</p>
-    </div>
-  </div>
-);
+  )
+}
 
 Intro.propTypes = {
   showGalery: PropTypes.func.isRequired,
