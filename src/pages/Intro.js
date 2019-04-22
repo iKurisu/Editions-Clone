1c7ffff79904012730e72c5e62c8ddd558020f42
iKurisu <chris.epic01@gmail.com>
2019-04-22 19:40:28 +0200

    Added animations

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,7 +4,22 @@ import "./Intro.scss";
 
 const Intro = ({ showGalery, atIntro }) => {
   const [display, setDisplay] = useState('block');
+  const [hlStyle, setHlStyle] = useState({ transform: 'translateY(10px)', opacity: 0 });
+  const [detailsStyle, setDetailsStyle] = useState({ opacity: 0 });
   
+  useEffect(() => {
+    const { opacity } = hlStyle;
+    
+    setHlStyle({ 
+      transform: `translateY(${opacity === 0 ? '0' : '-10px'})`, 
+      opacity: opacity === 0 ? 1 : 0 
+    });
+
+    setDetailsStyle({
+      opacity: opacity === 0 ? 1 : 0
+    })
+  }, [atIntro])
+
   useEffect(() => {
     !atIntro && setTimeout(() => setDisplay('none'), 900)
   });
@@ -15,17 +30,17 @@ const Intro = ({ showGalery, atIntro }) => {
       style={{ display: display }}
       onClick={showGalery}
     >
-      <div className="top text-center intro">
+      <div className="top text-center intro" style={detailsStyle}>
         <p>Selected Artwork</p>
       </div>
       <div className="vertical-container">
         <div className="headline-container">
-          <div className="headline">
+          <div className="headline" style={hlStyle}>
             <span>Editions</span>
           </div>
         </div>
       </div>
-      <div className="bottom text-center intro">
+      <div className="bottom text-center intro" style={detailsStyle}>
         <p>2016 – Present</p>
       </div>
     </div>
