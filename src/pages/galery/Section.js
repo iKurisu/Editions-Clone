00ab8341945cf9805d621617f63d0dbe48b34632
iKurisu <chris.epic01@gmail.com>
2019-04-22 19:43:20 +0200

    Update sections

--- src/pages/galery/Section.js
+++ src/pages/galery/Section.js
@@ -1,37 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import withMovement from './section/withMovement';
 import Details from "./section/Details";
 import Image from "./section/Image";
 import Title from "./section/Title";
 import "./Section.scss";
 
-const Section = ({ artwork, atIntro }) => {
+const Section = ({ artwork, x, y, mouseMove }) => {
   const { src, orientation, title, colors, details } = artwork;
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
-  const getOffset = (position, size, range) => {
-    const percent = (position * 100) / size;
-    return (range * percent) / 100 - range / 2;
-  };
-
-  const mouseMove = e => {
-    const { clientX, clientY } = e;
-    const { innerWidth, innerHeight } = window;
-
-    setX(getOffset(clientX, innerWidth, 28));
-    setY(getOffset(clientY, innerHeight, 18));
-  };
 
   return (
-    <section className={`section`} onMouseMove={mouseMove}>
+    <section 
+      className='section' 
+      onMouseMove={mouseMove}
+    >
       <div className="content-wrapper">
-        <Image src={src} orientation={orientation} atIntro={atIntro} />
+        <Image src={src} orientation={orientation} />
         <Title
           title={title}
           style={{
             transform: `translate(${x}px, ${y}px)`,
-            color: colors.font
+            color: colors.font,
           }}
         />
         <Details {...details} />
@@ -42,7 +31,9 @@ const Section = ({ artwork, atIntro }) => {
 
 Section.propTypes = {
   artwork: PropTypes.object.isRequired,
-  atIntro: PropTypes.bool
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  mouseMove: PropTypes.func.isRequired
 };
 
-export default Section;
+export default withMovement(Section);
--- src/pages/galery/section/Image.js
+++ src/pages/galery/section/Image.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import "./Image.scss";
 
-const Image = ({ src, orientation, atIntro }) => {
-  const [scale, setScale] = useState(atIntro ? 1.06 : 1.03);
-  const [opacity, setOpacity] = useState(atIntro ? 0 : 1);
-
-  useEffect(() => {
-    setScale(atIntro ? 1.03 : 1);
-  })
-
-  useEffect(() => {
-    setOpacity(1);
-  }, []);
-
+const Image = ({ src, orientation }) => {
   return (
-    <div
-      className={`image-wrapper ${orientation}`}
-      style={{ transform: `scale(${scale})`, opacity: opacity }}
-    >
+    <div className={`image-wrapper ${orientation}`}>
       <img src={src} />
     </div>
   );
@@ -26,8 +12,7 @@ const Image = ({ src, orientation, atIntro }) => {
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
-  orientation: PropTypes.string.isRequired,
-  atIntro: PropTypes.bool
+  orientation: PropTypes.string.isRequired
 }
 
 export default Image;
