3226f30401de9eed333ef99f6200b029ef5da596
iKurisu <chris.epic01@gmail.com>
2019-04-23 13:20:30 +0200

    Add utility to change animation values responsively

--- src/pages/galery/section/FirstImage.js
+++ src/pages/galery/section/FirstImage.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import mediaQuery from 'utils/mediaQuery';
 import "./Image.scss";
 
+const preIntroScale = mediaQuery({
+  "(max-width: 768px)": 0.94
+});
+
+const introScale = mediaQuery({
+  "(max-width: 768px)": 0.89
+});
+
 const FirstImage = ({ src, orientation, atIntro }) => {
-  const [scale, setScale] = useState(atIntro ? 1.06 : 1.03);
+  const [scale, setScale] = useState(atIntro ? preIntroScale : introScale);
   const [opacity, setOpacity] = useState(atIntro ? 0 : 1);
 
   useEffect(() => {
-    setScale(atIntro ? 1.03 : 1);
+    setScale(atIntro ? introScale : 1);
   });
 
   useEffect(() => {
