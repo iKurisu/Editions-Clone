5be6314c7d211b48835dfef0cde1df7bb27f0da6
iKurisu <chris.epic01@gmail.com>
2019-04-28 10:14:18 +0200

    Handle responsive size with js instead of css

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import fontSize from 'utils/fontSize';
+import mediaQuery from 'utils/mediaQuery';
 import "./Intro.scss";
 
 const Intro = ({ showGalery, atIntro }) => {
   const [display, setDisplay] = useState('block');
   const [hlStyle, setHlStyle] = useState({ transform: 'translateY(10px)', opacity: 0 });
   const [detailsStyle, setDetailsStyle] = useState({ opacity: 0 });
-  
+  const textSize = mediaQuery({
+    "(max-width: 768px)": fontSize('27vw', '31vh'),
+    "(min-width: 768px)": fontSize('24vw', '30.5vh'),
+    "(min-width: 1000px)": fontSize('18.4vw', '30.5vh'),
+    "(min-width: 1366px)": fontSize('17.5vw', '30.8vh')
+  });
+
   useEffect(() => {
     const { opacity } = hlStyle;
     
@@ -14,11 +22,10 @@ const Intro = ({ showGalery, atIntro }) => {
       transform: `translateY(${opacity === 0 ? '0' : '-10px'})`, 
       opacity: opacity === 0 ? 1 : 0 
     });
-
     setDetailsStyle({
-      opacity: opacity === 0 ? 1 : 0
-    })
-  }, [atIntro])
+      opacity: opacity === 0 ? 1 : 0 
+    });
+  }, [atIntro]);
 
   useEffect(() => {
     !atIntro && setTimeout(() => setDisplay('none'), 900)
@@ -36,7 +43,7 @@ const Intro = ({ showGalery, atIntro }) => {
       <div className="vertical-container">
         <div className="headline-container">
           <div className="headline" style={hlStyle}>
-            <span>Editions</span>
+            <span style={{ fontSize: textSize }}>Editions</span>
           </div>
         </div>
       </div>
