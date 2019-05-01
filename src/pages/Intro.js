4a61aedc431f2a309ffad032ba3fca4678be91d6
iKurisu <chris.epic01@gmail.com>
2019-05-01 18:01:12 +0200

    Connect intro to redux store

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+import withMovement from 'components/withMovement';
 import fontSize from 'utils/fontSize';
 import mediaQuery from 'utils/mediaQuery';
+import { introActions } from 'modules/intro';
 import "./Intro.scss";
 
 const textSize = mediaQuery({
@@ -11,16 +15,11 @@ const textSize = mediaQuery({
   "(min-width: 1366px)": fontSize("17.3vw", "30.8vh"),
   "(min-width: 1920px)": fontSize("15.3vw", "30.5vh")
 });
+
+const Intro = ({ toggled, translate, toggle, displace, mouseMove }) => {
   const [display, setDisplay] = useState('block');
   const [hlStyle, setHlStyle] = useState({ transform: 'translateY(10px)', opacity: 0 });
   const [detailsStyle, setDetailsStyle] = useState({ opacity: 0 });
-  const textSize = mediaQuery({
-    "(max-width: 768px)": fontSize('27vw', '31vh'),
-    "(min-width: 768px)": fontSize('24vw', '30.5vh'),
-    "(min-width: 1000px)": fontSize('18.4vw', '30.5vh'),
-    "(min-width: 1366px)": fontSize('17.3vw', '30.8vh'),
-    "(min-width: 1920px)": fontSize('15.3vw', '30.5vh')
-  });
 
   useEffect(() => {
     const { opacity } = hlStyle;
@@ -32,17 +31,23 @@ const textSize = mediaQuery({
     setDetailsStyle({
       opacity: opacity === 0 ? 1 : 0 
     });
-  }, [atIntro]);
+  }, [toggled]);
 
   useEffect(() => {
-    !atIntro && setTimeout(() => setDisplay('none'), 900)
+    !toggled && setTimeout(() => setDisplay('none'), 900)
   });
+
+  const handleMouseMove = e => {
+    mouseMove(e);
+    displace(translate.x, translate.y);
+  }
   
   return (
     <div 
       className="intro-container" 
       style={{ display: display }}
-      onClick={showGalery}
+      onClick={toggle}
+      onMouseMove={handleMouseMove}
     >
       <div className="top text-center intro" style={detailsStyle}>
         <p>Selected Artwork</p>
@@ -62,8 +67,20 @@ const textSize = mediaQuery({
 }
 
 Intro.propTypes = {
-  showGalery: PropTypes.func.isRequired,
-  atIntro: PropTypes.bool.isRequired
+  toggled: PropTypes.bool.isRequired,
+  translate: PropTypes.object.isRequired,
+  toggle: PropTypes.func.isRequired,
+  displace: PropTypes.func.isRequired,
+  mouseMove: PropTypes.func.isRequired
 }
 
-export default Intro;
+const mapState = ({ intro }) => ({ 
+  toggled: intro.toggled, 
+  displacement: intro.displacement 
+});
+const actionCreators = {
+  toggle: introActions.toggle,
+  displace: introActions.displace
+}
+
+export default connect(mapState, actionCreators)(withMovement(Intro));
