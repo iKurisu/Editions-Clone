48eabc6b96ee42f0fe82fa5659e412ad518bae6d
iKurisu <chris.epic01@gmail.com>
2019-05-01 18:01:30 +0200

    Connect blackPannel to redux store

--- src/components/BlackPanel.js
+++ src/components/BlackPanel.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { blackPanelActions } from 'modules/blackPanel';
 import "./BlackPanel.scss";
 
-const BlackPanel = ({ position }) => (
-  <div className="black-pannel" style={{ top: position }} />
-)
-
-BlackPanel.propTypes = {
-  position: PropTypes.string
+const BlackPanel = ({ toggled, position, setPosition }) => {  
+  useEffect(() => () => {
+    setPosition('-100%')
+  }, [toggled])
+  
+  return (
+    <div className="black-pannel" style={{ top: position }} />
+  )
 }
 
-export default BlackPanel;
+BlackPanel.propTypes = {
+  toggled: PropTypes.bool.isRequired,
+  position: PropTypes.string.isRequired,
+  setPosition: PropTypes.func.isRequired
+}
+
+const mapState = ({ intro, blackPanel }) => ({
+  toggled: intro.toggled,
+  position: blackPanel.position,
+
+})
+const actionCreators = {
+  setPosition: blackPanelActions.setPosition
+}
+
+export default connect(mapState, actionCreators)(BlackPanel);
