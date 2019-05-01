9bef3be1408c04c3ceeafbda6c3365d5e0c41c97
iKurisu <chris.epic01@gmail.com>
2019-05-01 17:55:03 +0200

    Add modules folder to resolve

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = {
       assets: path.resolve(__dirname, './src/assets'),
       components: path.resolve(__dirname, './src/components'),
       pages: path.resolve(__dirname, './src/pages'),
+      modules: path.resolve(__dirname, './src/modules'),
       utils: path.resolve(__dirname, './src/utils'),
       sass: path.resolve(__dirname, "./src/sass")
     }
