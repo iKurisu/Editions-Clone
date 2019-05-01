8e4aa6f09e130c45885e7a720505f2c425720add
iKurisu <chris.epic01@gmail.com>
2019-05-01 17:59:47 +0200

    Move App component into its own file

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Headers from 'components/Headers';
+import BlackPanel from 'components/BlackPanel';
+import Galery from 'pages/Galery';
+import Intro from 'pages/Intro';
+
+import artworks from './artworks';
+import "./styles.scss";
+
+const App = ({ toggled, displacement }) => (
+  <main className={toggled ? "intro" : ""}>
+    <Headers />
+    <Intro />
+    <Galery
+      artworks={artworks}
+      atIntro={toggled}
+      displacement={displacement}
+    />
+    <BlackPanel />
+  </main>
+);
+
+App.propTypes = {
+  toggled: PropTypes.bool.isRequired,
+  displacement: PropTypes.object.isRequired 
+}
+
+const mapState = ({ intro }) => ({ 
+  toggled: intro.toggled, 
+  displacement: intro.displacement 
+});
+
+export default connect(mapState)(App);
--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import * as PIXI from 'pixi.js';
-import Headers from 'components/Headers';
-import BlackPanel from 'components/BlackPanel';
-import Galery from 'pages/Galery';
-import Intro from 'pages/Intro';
-import artworks from './artworks';
-import "./styles.scss";
+import { Provider } from 'react-redux';
+import App from './App.js'
+import store from './store';
 
 let type = "WebGL";
 if (!PIXI.utils.isWebGLSupported()) type = "canvas";
 
 PIXI.utils.sayHello(type)
 
-const App = () => {
-  const [atIntro, setIntro] = useState(true);
-  const [panelPosition, setPanelPosition] = useState('0');
-  
-  const fadeIntro = () => {
-    setIntro(false);
-    setPanelPosition('-100%');
-  }
-
-  return (
-    <main className={atIntro ? 'intro' : ''}>
-      <Headers />
-      <Intro showGalery={fadeIntro} atIntro={atIntro} />
-      <Galery artworks={artworks} atIntro={atIntro} />
-      <BlackPanel position={panelPosition} />
-    </main>
-  );
-}
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
