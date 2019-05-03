eec1f2685c80736809758332079d419ff566daba
iKurisu <chris.epic01@gmail.com>
2019-05-03 09:42:43 +0200

    Connect galery to redux

--- src/pages/Galery.js
+++ src/pages/Galery.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { generate } from 'shortid';
 import Section from './galery/Section';
 import FirstSection from './galery/FirstSection';
 import artworks from "assets/artworks";
 import "./Galery.scss";
 
-const Galery = ({ atIntro }) => (
+const Galery = ({ toggled }) => (
   <div className="galery">
-    <FirstSection artwork={artworks[0]} atIntro={atIntro} />
-    {!atIntro && (
+    <FirstSection artwork={artworks[0]} atIntro={toggled} />
+    {!toggled && (
       <>
         {artworks.slice(1).map(artwork => (
           <Section artwork={artwork} key={generate()} />
@@ -21,7 +22,9 @@ const Galery = ({ atIntro }) => (
 );
 
 Galery.propTypes = {
-  atIntro: PropTypes.bool
+  toggled: PropTypes.bool.isRequired
 }
 
-export default Galery;
+const mapState = ({ intro }) => ({ toggled: intro.toggled });
+
+export default connect(mapState)(Galery);
