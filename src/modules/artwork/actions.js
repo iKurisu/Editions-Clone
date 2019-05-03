cabdddd0b4951700133ce383589afbed3c7e7b29
iKurisu <chris.epic01@gmail.com>
2019-05-03 20:29:43 +0200

    Add artwork reducers

---
+++ src/modules/artwork/actions.js
@@ -0,0 +1,9 @@
+import types from './types';
+
+const toggle = () => ({
+  type: types.TOGGLE
+});
+
+export default {
+  toggle
+}
---
+++ src/modules/artwork/index.js
@@ -0,0 +1,5 @@
+import reducer from './reducers';
+
+export { default as artworkActions } from './actions';
+
+export default reducer;
