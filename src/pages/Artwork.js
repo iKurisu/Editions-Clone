1f041dda5c3e645b88fcfe323871b8bc41635c55
iKurisu <chris.epic01@gmail.com>
2019-05-04 08:28:32 +0200

    Add some structure to artwork

--- src/pages/Artwork.js
+++ src/pages/Artwork.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { introActions } from 'modules/intro';
 import { artworkActions } from 'modules/artwork';
+import "./Artwork.scss";
 
 const Artwork = ({ artwork, toggleIntro, toggleArtwork }) => {
   useEffect(() => {
@@ -11,7 +12,20 @@ const Artwork = ({ artwork, toggleIntro, toggleArtwork }) => {
   }, []);
 
   return (
-    <div className="artwork">
+    <div className="artwork" style={{ background: artwork.colors.background }}>
+      <div className="artwork-details">
+        <div className="details-container">
+          <h1 className="details-title">{artwork.title}</h1>
+          <p>
+            From <span>${artwork.details.price}.00</span>
+            <span>—</span>
+            <a>Buy Now</a>
+          </p>
+          <div>
+            <img src={artwork.src} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
