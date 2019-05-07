be5e19b3920c23007dc5cb89f9cd99ba52b86657
iKurisu <chris.epic01@gmail.com>
2019-05-07 11:06:00 +0200

    Add remaining sections from artwork

--- src/pages/Artwork.js
+++ src/pages/Artwork.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { generate } from 'shortid';
 import { introActions } from 'modules/intro';
 import { artworkActions } from 'modules/artwork';
 import "./Artwork.scss";
 
 const Artwork = ({
-  artwork: { colors, title, details, src },
+  artwork: { colors, title, details, src, formats },
   toggleIntro,
   toggleArtwork
 }) => {
   const { innerWidth: width } = window;
+  const [format, setFormat] = useState(formats[0]);
   
+  const updateFormat = format => () => setFormat(format); 
+
   useEffect(() => {
     toggleIntro();
     toggleArtwork();
@@ -23,7 +27,7 @@ const Artwork = ({
         <div className="details-container">
           <h1 className="details-title">{title}</h1>
           <p>
-            From <span>${details.price}.00</span>
+            From <span>${format.price}.00</span>
             <span className="dash">—</span>
             <a>Buy Now</a>
           </p>
@@ -32,12 +36,40 @@ const Artwork = ({
           </div>
         </div>
       </div>
-      <section>
+      <section className="artwork-preview">
         <figure>
-          <img
-            src={width < 768 ? src.details_small : src.details}
-          />
+          <img src={width < 768 ? src.details_small : src.details} />
         </figure>
+      </section>
+      <section className="artwork-purchase">
+        <div className="artwork-purchase-container">
+          <div className="artwork-framed flex">
+            <img src={src.framed} />
+          </div>
+          <section className="flex">
+            <div className="artwork-description">
+              <h1 style={{ color: colors.background }}>{title}</h1>
+              <p>{details.type} — Edition of 30</p>
+              <p>
+                Giclée fine art print on premium archival smooth paper, 290
+                gsm, 21.5 mil. Print includes a white border for framing.
+                Frame not included.
+              </p>
+              <ul>
+                { formats.map(format => (
+                  <li key={generate()} onClick={updateFormat(format)}>{format.dimensions} in</li>
+                ))}
+              </ul>
+              <button>
+                <span>
+                  <span>Add to Cart</span>
+                  <span className="dollar">$</span>
+                  <span>{format.price}.00</span>
+                </span>
+              </button>
+            </div>
+          </section>
+        </div>
       </section>
     </div>
   );
