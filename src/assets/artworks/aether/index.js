6244202bee05364ab541c52d8de5b6844911a522
iKurisu <chris.epic01@gmail.com>
2019-05-09 18:47:14 +0200

    Add framed artwork for small screens

--- src/assets/artworks/aether/index.js
+++ src/assets/artworks/aether/index.js
@@ -2,10 +2,12 @@ import main from "./main.jpg";
 import details from "./details.jpg";
 import framed from "./framed.jpg";
 import details_small from './details_small.jpg';
+import framed_small from './framed-small.jpg';
 
 export default {
   main,
   details,
   details_small,
-  framed
+  framed,
+  framed_small
 }
