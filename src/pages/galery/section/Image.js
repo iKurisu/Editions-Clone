e35d3a44168dc0ff268eb5c5e66d1a1266739ff7
iKurisu <chris.epic01@gmail.com>
2019-05-15 08:35:22 +0200

    Add transition from galery to artwork

--- src/pages/galery/section/Image.js
+++ src/pages/galery/section/Image.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import "./Image.scss";
+import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+import { blackPanelActions } from 'modules/blackPanel';
+import "./Image.scss"; 
 
-const Image = ({ src, title, orientation }) => {
+const Image = ({ src, title, orientation, history, setPosition }) => {
+  const route = `/${title.toLowerCase()}`;
+  
+  const goTo = e => {
+    e.preventDefault();
+    setPosition('-100%');
+    setTimeout(() => history.push(route), 900);
+  }
+  
   return (
     <div className={`image-wrapper ${orientation}`}>
-      <Link to={`/${title.toLowerCase()}`}>
+      <Link to={route} onClick={goTo}>
         <img src={src} />
       </Link>
     </div>
@@ -16,7 +26,13 @@ const Image = ({ src, title, orientation }) => {
 Image.propTypes = {
   src: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  orientation: PropTypes.string.isRequired
+  orientation: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
+  setPosition: PropTypes.func.isRequired
 }
 
-export default Image;
+const actionCreators = {
+  setPosition: blackPanelActions.setPosition
+}
+
+export default withRouter(connect(null, actionCreators)(Image));
