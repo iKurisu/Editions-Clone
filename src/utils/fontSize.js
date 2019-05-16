4499a7fdcc587eeb98079fd2ee3c93f7305b41d9
iKurisu <chris.epic01@gmail.com>
2019-05-16 07:58:15 +0200

    Merge files

--- src/utils/fontSize.js
+++ src/utils/fontSize.js
@@ -1,4 +1,17 @@
-import getPixels from "./getPixels";
+const getPixels = x => {
+  const { innerWidth, innerHeight } = window;
+  const value = x.slice(0, x.length - 2);
+  const unit = x.slice(x.length - 2);
+
+  switch (unit) {
+    case "vw":
+      return (value * innerWidth) / 100;
+    case "vh":
+      return (value * innerHeight) / 100;
+    default:
+      return value;
+  }
+};
 
 const fontSize = (fontSize, maxFontSize = fontSize) => {
   return getPixels(fontSize) > getPixels(maxFontSize) ? maxFontSize : fontSize;
