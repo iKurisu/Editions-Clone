3b6555a54a0afafb86d75a22e9b8d476e8fb0afe
iKurisu <chris.epic01@gmail.com>
2019-05-26 18:05:57 +0200

    Add isclone prp

--- src/pages/galery/section/Image.js
+++ src/pages/galery/section/Image.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import LinkedImage from './image/LinkedImage';
 import "./Image.scss"; 
 
-const Image = ({ src, title, orientation }) => (
+const Image = ({ src, title, orientation, isClone }) => (
   <div className={`image-wrapper ${orientation}`}>
-    <LinkedImage src={src} title={title} />
+    <LinkedImage src={src} title={title} isClone={isClone} />
   </div>
 );
 
@@ -13,6 +13,7 @@ Image.propTypes = {
   src: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   orientation: PropTypes.string.isRequired,
+  isClone: PropTypes.bool
 }
 
 export default Image;
