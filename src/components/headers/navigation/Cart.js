8a909f635cd7837ec3fa82742f4ba01ca0d1c129
iKurisu <chris.epic01@gmail.com>
2019-05-27 18:40:09 +0200

    Add click event to cart button

--- src/components/headers/navigation/Cart.js
+++ src/components/headers/navigation/Cart.js
@@ -1,8 +1,22 @@
 import React from "react";
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { cartActions } from 'modules/cart';
 import './Cart.scss';
 
-const Cart = () => <div className="cart-button button top-right" />;
+const Cart = ({ toggle }) => <div 
+  className="cart-button button top-right" 
+  onClick={toggle}
+/>;
+
+Cart.propTypes = {
+  toggle: PropTypes.func.isRequired
+}
 
 const areEqual = () => true;
 
-export default React.memo(Cart, areEqual);
+const actionCreators = {
+  toggle: cartActions.toggle
+}
+
+export default React.memo(connect(null, actionCreators)(Cart), areEqual);
