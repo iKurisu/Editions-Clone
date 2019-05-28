40045cd56145b3bbf4c72e4617bef6acabf88ee9
iKurisu <chris.epic01@gmail.com>
2019-05-28 08:23:58 +0200

    Add empty items component

---
+++ src/components/cart/Items.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Items.scss';
+
+const Items = () => (
+  <div className="items-container">
+    <p className="empty-text">Your cart is currently empty.</p>
+  </div>
+)
+
+export default Items;
