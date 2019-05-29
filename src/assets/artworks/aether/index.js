862cb6e9454c5493c97ed8e7900e87e0c99f8da6
iKurisu <chris.epic01@gmail.com>
2019-05-29 11:36:02 +0200

    Add artwork images for cart

--- src/assets/artworks/aether/index.js
+++ src/assets/artworks/aether/index.js
@@ -1,4 +1,5 @@
 import main from "./main.jpg";
+import main_small from './main_small.jpg';
 import details from "./details.jpg";
 import framed from "./framed.jpg";
 import details_small from './details_small.jpg';
@@ -6,6 +7,7 @@ import framed_small from './framed-small.jpg';
 
 export default {
   main,
+  main_small,
   details,
   details_small,
   framed,
