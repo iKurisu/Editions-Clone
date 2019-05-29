feedcb6b01cf3d646d3bc606b6e1d75c3a983045
iKurisu <chris.epic01@gmail.com>
2019-05-29 11:41:02 +0200

    Connect cart item container to redux

--- src/components/cart/Items.js
+++ src/components/cart/Items.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { generate } from 'shortid';
+import Item from './Item';
 import './Items.scss';
 
-const Items = () => (
+const Items = ({ items }) => (
   <div className="items-container">
     <p className="empty-text">Your cart is currently empty.</p>
+    { items.map(item => <Item key={generate()} artwork={item} />)}
   </div>
 )
 
-export default Items;
+Items.propTypes = {
+  items: PropTypes.array.isRequired
+}
+
+const mapState = ({ cart }) => ({
+  items: cart.items
+})
+
+export default connect(mapState)(Items);
