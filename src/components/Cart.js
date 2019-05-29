d1c8fac6710f4e697f8ad0d0c0d9cd97d6c79e48
iKurisu <chris.epic01@gmail.com>
2019-05-29 11:43:36 +0200

    Hide empty text when cart has items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,26 +4,33 @@ import { connect } from 'react-redux';
 import { cartActions } from 'modules/cart';
 import Header from './cart/Header';
 import Items from './cart/Items';
+import Checkout from './cart/Checkout';
 import './Cart.scss';
 
-const Cart = ({ toggle }) => {
+const Cart = ({ items, toggle }) => {
   return (
-    <div className="cart">
+    <div className={`cart ${items.length > 0 ? '': 'empty'}`}>
       <div className="cart-overlay" onClick={toggle} />
       <section className="cart-container">
         <Header toggle={toggle} />
         <Items />
+        <Checkout />
       </section>
     </div>
   )
 }
 
 Cart.propTypes = {
+  items: PropTypes.array.isRequired,
   toggle: PropTypes.func.isRequired
 }
+
+const mapState = ({ cart }) => ({
+  items: cart.items
+})
 
 const actionCreators = {
   toggle: cartActions.toggle
 }
 
-export default connect(null, actionCreators)(Cart);
+export default connect(mapState, actionCreators)(Cart);
