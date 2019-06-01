c24a671759a94292992fe57bdfef3dfd0f6e1803
iKurisu <chris.epic01@gmail.com>
2019-06-01 13:11:58 +0200

    Add item amount to cart button

--- src/components/headers/navigation/Cart.js
+++ src/components/headers/navigation/Cart.js
@@ -4,19 +4,43 @@ import { connect } from 'react-redux';
 import { cartActions } from 'modules/cart';
 import './Cart.scss';
 
-const Cart = ({ toggle }) => <div 
-  className="cart-button button top-right" 
-  onClick={toggle}
-/>;
+const Cart = ({ items, toggle }) => {
+  const itemAmount = items.reduce((total, item) => {
+    return total + item.amount
+  }, 0); 
+
+  return (
+    <div 
+      className={
+        `cart-button button top-right ${itemAmount > 0 ? 'show-amount' : ''}`
+      } 
+      onClick={toggle}
+    >
+      <p className="cart-button-amount">{itemAmount ? itemAmount : ''}</p>
+      <span />
+    </div>
+  );
+}
 
 Cart.propTypes = {
+  items: PropTypes.array.isRequired,
   toggle: PropTypes.func.isRequired
 }
 
-const areEqual = () => true;
+const areEqual = (prevProps, nextProps) => {
+  if (prevProps.hasOwnProperty('items')) {
+    return prevProps.items.length === nextProps.items.length;
+  }
+
+  return true;
+}
+
+const mapState = ({ cart }) => ({
+  items: cart.items
+})
 
 const actionCreators = {
   toggle: cartActions.toggle
 }
 
-export default React.memo(connect(null, actionCreators)(Cart), areEqual);
+export default React.memo(connect(mapState, actionCreators)(Cart), areEqual);
